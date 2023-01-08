c2c55929359d4a89c75cfb0de34c6c07fef41faf
Athena Xu <117648445+athenax246@users.noreply.github.com>
2023-01-07 23:43:28 -0500

    Update 02 - Old Version with Comments.py

--- 02 - Old Version with Comments.py
+++ 02 - Old Version with Comments.py
@@ -1,29 +1,3 @@
-#Hello! Welcome to our project :)
-# Source: from inventwithpython.com, buggy programs
-# This version of the game has a bug in it. See if you can figure out how to fix it.
-# http://inventwithpython.com/pygame/buggy
-# Bug Description: When dropping a token on a tall stack, the token appears to drop past the top token on the stack.
-
-# Four-In-A-Row (a Connect Four clone)
-# By Al Sweigart al@inventwithpython.com
-# http://inventwithpython.com/pygame
-# Released under a "Simplified BSD" license
-
-#Current distribution: (these are assigned with random name selector, feel free to change anything!)
-#Part 1 - Juman
-#Part 2 - Athena
-#Part 3 - Kohinoor
-#Part 4 - Iris
-#The parts are commented on the source code
-
-# ------- first person starts here ********* Juman
-
-# importing four modules:
-#random- used to generate random numbers/moves
-#copy- allows to copy lists and use those copies
-#sys- access files within system (used to get png files)
-#pygame- used to run window
-
 import random, copy, sys, pygame
 
 #importing everything from pygame.locals
@@ -146,7 +120,7 @@ def runGame(isFirstGame):
             winnerImg = TIEWINNERIMG
             break
 
-# ------- second person starts here ********* Athena
+
     while True:
         # the while True loop keep repeating the code inside it until the player clicks the mouse or quits.
         drawBoard(mainBoard) #create the mainboard
@@ -268,7 +242,7 @@ def getHumanMove(board, isFirstMove):
                 tokenx, tokeny = None, None #reset token location variables
                 draggingToken = False #reset dragging variable
 
-# ------ person 3 starts comments ****** Kohinoor
+
               
         if tokenx != None and tokeny != None: #if both positions of the token have a value
             drawBoard(board, {'x':tokenx - int(SPACESIZE / 2), 'y':tokeny - int(SPACESIZE / 2), 'color':RED}) #Draws the token on the board after a location on the board has been decided
@@ -384,7 +358,7 @@ def getPotentialMoves(board, tile, lookAhead):
             continue #if they are, continue
         makeMove(dupeBoard, tile, firstMove) #makes a move on the copy board
 
-# ------ person 4 comments here ****** Iris
+
       
         if isWinner(dupeBoard, tile):
             #look at the deepcopy of the board. If the computer's move is a winning move,
